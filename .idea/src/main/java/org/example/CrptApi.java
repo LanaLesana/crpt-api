0437d8339716c2823bce70aa2a165d77f53773aa
Olesia <olesia04@yandex.ru>
2024-04-10 13:32:39 +0400

    crptApi

---
+++ .idea/src/main/java/org/example/CrptApi.java
@@ -0,0 +1,301 @@
+package org.example;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class CrptApi {
+    private final Semaphore rateLimiter;
+    private final HttpClient httpClient;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public CrptApi(TimeUnit timeUnit, int requestLimit) {
+        this.rateLimiter = new Semaphore(requestLimit);
+        this.httpClient = HttpClient.newHttpClient();
+
+        new Thread(() -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    timeUnit.sleep(1);
+                    rateLimiter.release(requestLimit - rateLimiter.availablePermits());
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }).start();
+    }
+
+    public void createDocument(Document doc, String signature) throws InterruptedException, IOException {
+        try {
+            rateLimiter.acquire();
+
+            String requestBody = toJson(doc, signature);
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://ismp.crpt.ru/api/v3/lk/documents/create"))
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                    .build();
+
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println("Response status code: " + response.statusCode());
+            System.out.println("Response body: " + response.body());
+        } finally {
+            rateLimiter.release();
+        }
+    }
+
+    private String toJson(Document doc, String signature) throws JsonProcessingException {
+        doc.setSignature(signature);
+        return objectMapper.writeValueAsString(doc);
+    }
+
+    public static class Document {
+        private String description;
+        @JsonProperty("doc_id")
+        private String docId;
+        @JsonProperty("doc_status")
+        private String docStatus;
+        @JsonProperty("doc_type")
+        private String docType;
+        @JsonProperty("importRequest")
+        private boolean importRequest;
+        @JsonProperty("owner_inn")
+        private String ownerInn;
+        @JsonProperty("participant_inn")
+        private String participantInn;
+        @JsonProperty("producer_inn")
+        private String producerInn;
+        @JsonProperty("production_date")
+        private String productionDate;
+        @JsonProperty("production_type")
+        private String productionType;
+        private List<Product> products;
+        @JsonProperty("reg_date")
+        private String regDate;
+        @JsonProperty("reg_number")
+        private String regNumber;
+        private String signature;
+
+        public String getDescription() {
+            return description;
+        }
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public void setSignature(String signature) {
+            this.signature = signature;
+        }
+        public String getDocId() {
+            return docId;
+        }
+
+        public void setDocId(String docId) {
+            this.docId = docId;
+        }
+
+        public String getDocStatus() {
+            return docStatus;
+        }
+
+        public void setDocStatus(String docStatus) {
+            this.docStatus = docStatus;
+        }
+
+        public String getDocType() {
+            return docType;
+        }
+
+        public void setDocType(String docType) {
+            this.docType = docType;
+        }
+
+        public boolean isImportRequest() {
+            return importRequest;
+        }
+
+        public void setImportRequest(boolean importRequest) {
+            this.importRequest = importRequest;
+        }
+
+        public String getOwnerInn() {
+            return ownerInn;
+        }
+
+        public void setOwnerInn(String ownerInn) {
+            this.ownerInn = ownerInn;
+        }
+
+        public String getParticipantInn() {
+            return participantInn;
+        }
+
+        public void setParticipantInn(String participantInn) {
+            this.participantInn = participantInn;
+        }
+
+        public String getProducerInn() {
+            return producerInn;
+        }
+
+        public void setProducerInn(String producerInn) {
+            this.producerInn = producerInn;
+        }
+
+        public String getProductionDate() {
+            return productionDate;
+        }
+
+        public void setProductionDate(String productionDate) {
+            this.productionDate = productionDate;
+        }
+
+        public String getProductionType() {
+            return productionType;
+        }
+
+        public void setProductionType(String productionType) {
+            this.productionType = productionType;
+        }
+
+        public List<Product> getProducts() {
+            return products;
+        }
+
+        public void setProducts(List<Product> products) {
+            this.products = products;
+        }
+
+        public String getRegDate() {
+            return regDate;
+        }
+
+        public void setRegDate(String regDate) {
+            this.regDate = regDate;
+        }
+
+        public String getRegNumber() {
+            return regNumber;
+        }
+
+        public void setRegNumber(String regNumber) {
+            this.regNumber = regNumber;
+        }
+
+        public String getSignature() {
+            return signature;
+        }
+    }
+
+    public static class Product {
+        @JsonProperty("certificate_document")
+        private String certificateDocument;
+        @JsonProperty("certificate_document_date")
+        private String certificateDocumentDate;
+        @JsonProperty("certificate_document_number")
+        private String certificateDocumentNumber;
+        @JsonProperty("owner_inn")
+        private String ownerInn;
+        @JsonProperty("producer_inn")
+        private String producerInn;
+        @JsonProperty("production_date")
+        private String productionDate;
+        @JsonProperty("tnved_code")
+        private String tnvedCode;
+        @JsonProperty("uit_code")
+        private String uitCode;
+        @JsonProperty("uitu_code")
+        private String uituCode;
+
+        public String getCertificateDocument() {
+            return certificateDocument;
+        }
+
+        public void setCertificateDocument(String certificateDocument) {
+            this.certificateDocument = certificateDocument;
+        }
+
+        public String getCertificateDocumentDate() {
+            return certificateDocumentDate;
+        }
+
+        public void setCertificateDocumentDate(String certificateDocumentDate) {
+            this.certificateDocumentDate = certificateDocumentDate;
+        }
+
+        public String getCertificateDocumentNumber() {
+            return certificateDocumentNumber;
+        }
+
+        public void setCertificateDocumentNumber(String certificateDocumentNumber) {
+            this.certificateDocumentNumber = certificateDocumentNumber;
+        }
+
+        public String getOwnerInn() {
+            return ownerInn;
+        }
+
+        public void setOwnerInn(String ownerInn) {
+            this.ownerInn = ownerInn;
+        }
+
+        public String getProducerInn() {
+            return producerInn;
+        }
+
+        public void setProducerInn(String producerInn) {
+            this.producerInn = producerInn;
+        }
+
+        public String getProductionDate() {
+            return productionDate;
+        }
+
+        public void setProductionDate(String productionDate) {
+            this.productionDate = productionDate;
+        }
+
+        public String getTnvedCode() {
+            return tnvedCode;
+        }
+
+        public void setTnvedCode(String tnvedCode) {
+            this.tnvedCode = tnvedCode;
+        }
+
+        public String getUitCode() {
+            return uitCode;
+        }
+
+        public void setUitCode(String uitCode) {
+            this.uitCode = uitCode;
+        }
+
+        public String getUituCode() {
+            return uituCode;
+        }
+
+        public void setUituCode(String uituCode) {
+            this.uituCode = uituCode;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        CrptApi api = new CrptApi(TimeUnit.SECONDS, 5);
+        Document doc = new Document();
+        // Заполняем объект doc соответствующими данными
+        String signature = "signature";
+        api.createDocument(doc, signature);
+    }
+}
+
